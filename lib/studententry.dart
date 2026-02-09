bd28b8c592f6c9f13d3b7220b394ef67061d1489
Thasmeela <klthasmeela@gmail.com>
2026-02-09 12:51:24 +0530

    first commit

---
+++ lib/studententry.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Studententry extends StatelessWidget {
+  const Studententry({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
